5ad982f326a2bc4faaadd1e294ae65036ecf54f3
Bickolas Liu <51180693+liunicholas@users.noreply.github.com>
2020-12-10 15:56:45 -0800

    make window

--- ballShoot.py
+++ ballShoot.py
@@ -1,22 +1,71 @@
 from graphics import *
 
-def makeWindow():
-    width = 1200
-    height = 800
-    gw = GraphWin("ball trajectory", width, height)
-    gw.setCoords(0,0,width,height)
-    gw.setBackground("black")
+'''
+change variables here:
+'''
+initialVelocity = 20 #m/s
+initialAngle = 30 #degrees
+mass = 4 #kilograms
+timeInterval = 0.1 #seconds
 
-    text = Text(Point(80,780), "2D Kinematics With Air Resistance")
+
+width = 1300
+height = 800
+origin = Point(100,100)
+xAxisEnd = Point(1150,origin.getY())
+yAxisEnd = Point(origin.getX(),700)
+gw = GraphWin("ball trajectory", width, height)
+gw.setCoords(0,0,width,height)
+gw.setBackground("black")
+
+xCoords = []
+yCoords = []
+xVelocites = []
+yVelocities = []
+velocities = []
+xForces = []
+yForces = []
+forces = []
+times = []
+
+def makeText(text,x,y,color,size,font):
+    text = Text(Point(x,y), text)
     text.setFill("white")
-    text.setSize(20)
+    text.setSize(36)
+    text.setFace("arial")
     text.draw(gw)
 
-    return gw
+def makeWindowDetails():
+    makeText("2D Kinematics With Air Resistance",300,750,"white",36,"arial")
+
+    line = Line(origin,xAxisEnd)
+    line.setFill("white")
+    line.setOutline("white")
+    line.draw(gw)
+
+    line = Line(origin,yAxisEnd)
+    line.setFill("white")
+    line.setOutline("white")
+    line.draw(gw)
+
+def getVectorVelocity():
+
+    return
+
+def getVectorForce():
+
+    return
+
+def getPoints():
+
+    return
+
+def displayPoints():
+
+    return
 
 def main():
-
-    gw = makeWindow()
+    makeWindowDetails()
 
     click = gw.getMouse()
     gw.close()
