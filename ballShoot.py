3e7bf0d393ae3667682a12c6204b71a55bfff307
liunicholas <nicholasliu2015@gmail.com>
2020-12-11 11:15:42 -0800

    setup is complete

--- ballShoot.py
+++ ballShoot.py
@@ -1,4 +1,5 @@
 from graphics import *
+import math
 
 '''
 change variables here:
@@ -7,26 +8,31 @@ initialVelocity = 20 #m/s
 initialAngle = 30 #degrees
 mass = 4 #kilograms
 timeInterval = 0.1 #seconds
-
+k = 2 #combination of drag coefficient, cross sectional area, and density of fluid
 
 width = 1300
 height = 800
 origin = Point(100,100)
-xAxisEnd = Point(1150,origin.getY())
-yAxisEnd = Point(origin.getX(),700)
+xAxisEnd = Point(1150,100)
+yAxisEnd = Point(100,700)
 gw = GraphWin("ball trajectory", width, height)
 gw.setCoords(0,0,width,height)
 gw.setBackground("black")
 
-xCoords = []
-yCoords = []
-xVelocites = []
-yVelocities = []
-velocities = []
-xForces = []
-yForces = []
-forces = []
-times = []
+xNow = 0
+yNow = 0
+vXNow = math.cos(math.radians(initialAngle))*initialVelocity
+vYNow = math.sin(math.radians(initialAngle))*initialVelocity
+
+xCoords = [xNow]
+yCoords = [yNow]
+xVelocites = [vXNow]
+yVelocities = [vYNow]
+velocities = [math.sqrt(math.pow(vXNow,2)+math.pow(vYNow,2))]
+xForces = [math.pow(vXNow,2)*k]
+yForces = [math.pow(vYNow,2)*k]
+forces = [math.sqrt(math.pow(math.pow(vXNow,2)*k,2)+math.pow(math.pow(vYNow,2)*k,2))]
+times = [0]
 
 def makeText(text,x,y,color,size,font):
     text = Text(Point(x,y), text)
@@ -37,6 +43,13 @@ def makeText(text,x,y,color,size,font):
 
 def makeWindowDetails():
     makeText("2D Kinematics With Air Resistance",300,750,"white",36,"arial")
+
+    makeText("2D Kinematics With Air Resistance",300,750,"white",36,"arial")
+    makeText("Time:",950,800-30,"white",8,"arial")
+    makeText("Position:",950,750-30,"white",8,"arial")
+    makeText("Velocity:",950,700-30,"white",8,"arial")
+    makeText("Force:",950,650-30,"white",8,"arial")
+    makeText("Angle:",950,600-30,"white",8,"arial")
 
     line = Line(origin,xAxisEnd)
     line.setFill("white")
@@ -60,7 +73,13 @@ def getPoints():
 
     return
 
+def findScalingNumber():
+
+    return
+
 def displayPoints():
+    for i in range(len(xCoords)):
+        point = Point()
 
     return
 
